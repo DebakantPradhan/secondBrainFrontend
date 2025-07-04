805bfe63a3ec412ca5674dfe18853f0d3ea78849
DebakantPradhan <debakantpradhan646@gmail.com>
2025-07-04 13:44:46 +0530

    feat: Add landing page and environment configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
 import Auth from './pages/Auth';
 import Dashboard from './pages/Dashboard';
-
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
 
 function App() {
 	return (
 		<BrowserRouter>
-            <Routes>
-                <Route path='/signin' element={<Auth signin={true}/>} />
-           
-                <Route path='/signup' element={<Auth signup={true}/>} />
-            
-                <Route path='/dashboard' element={<Dashboard />} />
-            </Routes>
-        </BrowserRouter>
+			<Routes>
+				<Route path="/signin" element={<Auth signin={true} />} />
+				
+				<Route path="/" element={<LandingPage />} />
+
+				<Route path="/signup" element={<Auth signup={true} />} />
+
+				<Route path="/dashboard" element={<Dashboard />} />
+			</Routes>
+		</BrowserRouter>
 
 		// <>
 		//     <Dashboard />
